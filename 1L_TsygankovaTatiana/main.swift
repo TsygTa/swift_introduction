dcba46e6ea2ff1dc553cc22fd7b36c425d16b0c4
TsygTa <670724@gmail.com>
2018-09-27 11:12:07 +0300

    Initial Commit

---
+++ 1L_TsygankovaTatiana/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  1L_TsygankovaTatiana
+//
+//  Created by Цыганкова Татьяна on 27.09.2018.
+//  Copyright © 2018 Цыганкова Татьяна. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
